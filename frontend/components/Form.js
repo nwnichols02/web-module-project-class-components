2de3b4b6943a366793794ceb6073878636114fba
Nathan Nichols <nwnichols02@gmail.com>
2022-03-07 16:55:55 -0700

    can add and remove tasks with button

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,10 +1,30 @@
 import React from 'react'
 
 export default class Form extends React.Component {
+  onSubmit = evt => {
+    evt.preventDefault()
+    this.props.onSubmit()
+  }
+
+  onChange = evt => {
+    const {value, id} =evt.target
+    this.props.onChange(id, value)
+  }
   render() {
+    const { values } = this.props
     return (
       <div>
-        Form
+        <form onSubmit={this.onSubmit}>
+        <input
+        value={values.textInput}
+        onChange={this.onChange}
+        type='text'
+        id='textInput'
+        placeholder='type to add a new task'
+        />  
+        <input type='submit'/>
+        </form>
+
       </div>
     )
   }
